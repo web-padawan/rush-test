85aad96154b4c715ddf2335c0a5c3e33dae9e55a
web-padawan <iamkulykov@gmail.com>
2020-06-08 16:43:33 +0300

    Cleanup test helpers and configs

--- mixins/element-base/web-test-runner.config.js
+++ mixins/element-base/web-test-runner.config.js
@@ -1,7 +1,1 @@
-const {
-  createEsDevServer
-} = require("web-test-runner/dist/implementations/servers/es-dev-server.js");
-
-module.exports = {
-  server: createEsDevServer({ preserveSymlinks: true }),
-};
+module.exports = {};
